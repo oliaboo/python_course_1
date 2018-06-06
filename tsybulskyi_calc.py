fd5d1fa89b32a41595467e9aff4816a85efc5052
Andrii Tsybulskyi <ua.ants@gmail.com>
2018-06-06 12:47:20 +0300

    calculator 2nd version, function

--- tsybulskyi_calc.py
+++ tsybulskyi_calc.py
@@ -1,37 +1,56 @@
-while True:
-	a = input('Enter A: ')
-	if a == 'q':
-		break
-	
-	b = input('Enter B: ')
-	if b == 'q':
-		break
-	
-	op = input('Enter operation: ')
-	if op == 'q':
-		break
-	
-	try:
-		int(a)
-		int(b)
-	except ValueError:
-		print('value is not integer')
-		continue
-	else:
-		a = int(a)
-		b = int(b)
+
+def operations(a, b, op):
+	"""function requires 3 paramters:
+	a - first operand, integre only
+	b - second operand, integer only
+	op - operation, shall be one of '+', '-', '/', '%', '**' """
 
 	if op == '+':
 		print('a + b =', a + b)
 	elif op == '-':
 		print('a - b =', a - b)
 	elif op == '/':
-		print('a / b =', a/b)
+		try:
+			print('a / b =', a/b)
+		except ZeroDivisionError:
+			print('Cannot devide by zero')
 	elif op == '%':
-		print('a % b =', a%b)
+		try:
+			print('a % b =', a%b)
+		except ZeroDivisionError:
+			print('Cannot devide by zero')
 	elif op == '**':
 		print('a ** b =', a**b)
 	else:
 		print("You've entered unknown oprtation")
 
 
+while True:
+	a = input('Enter A: ')
+	if a == 'q':
+		break
+	else:
+		try:
+			a = int(a)
+		except ValueError:
+			print('value is not integer')
+			continue
+
+	b = input('Enter B: ')
+	if b == 'q':
+		break
+	else:
+		try:
+			b = int(b)
+		except ValueError:
+			print('value is not integer')
+			continue
+
+	op = input('Enter operation: ')
+	if op == 'q':
+		break
+	elif op not in ['+', '-', '/', '%', '**']:
+		print('Unsupported opperation')
+		continue
+
+	operations(a, b, op)
