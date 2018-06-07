b92c45d3cc0cb499077540c06dcdc51ca058ab8c
Oleg Kit <oleg2448@gmail.com>
2018-06-07 09:32:31 +0300

    I have removed the crutch

--- Function Calculator.py
+++ Function Calculator.py
@@ -30,9 +30,57 @@ def divide(x, y):
 	    
 	return x / y
 
-operations = {'/': divide, '*': multiply, '+': add, '-': subtract}
+def calculation(raw_expression): 
 
-counter = 0 
+    for operation in operations:
+
+        if operation in raw_expression:                                                                             # checking for an operation in a string    
+
+            operation_index = raw_expression.index(operation)
+
+            if operation_index > 0 and operation_index < len(raw_expression):                                       # checking for an operation place in a string
+
+                if raw_expression[0:operation_index].isdigit():
+    
+                    x = float(raw_expression[0:operation_index])
+
+                else:
+
+                    break
+
+                if raw_expression[operation_index+1:expression_length].isdigit():
+
+                    y = float(raw_expression[operation_index+1:expression_length])
+                    
+                else:
+
+                    break
+
+                result = operations[operation](x, y)
+
+                print('\n')
+
+                if type(result) is float:
+
+                    return print("Result : ", "%.2f" % result, '\n')
+
+                else:
+
+                    return print(result, '\n')
+
+                break
+
+
+            else:
+
+                print('\nExpression Error!')
+
+                break
+
+    print('\nExpression Error!')
+
+
+operations = {'/': divide, '*': multiply, '+': add, '-': subtract} 
 
 print('Write an expression in one line. ex.: A*B')
 
@@ -52,55 +100,10 @@ while True:
 
     if expression_length >= 3:                                                                                          # checking for the minimum line size
 
-        for operation in operations:
-
-            if operation in raw_expression:                                                                             # checking for an operation in a string    
-
-                operation_index = raw_expression.index(operation)
-
-                if operation_index > 0 and operation_index < len(raw_expression):                                       # checking for an operation place in a string
-
-                    x = float(raw_expression[0:operation_index])
-
-                    y = float(raw_expression[operation_index+1:expression_length])
-
-                    result = operations[operation](x, y)
-
-                    print('\n')
-
-                    if type(result) is float:
-
-                        print("Result : ", "%.2f" % result, '\n')
-
-                    else:
-
-                        print(result, '\n')
-
-                    break
-
-
-                else:
-
-                    print('\nExpression Error!')
-
-                    break
-
-            else:
-
-                counter = counter + 1
-
-                if counter == 4:
-
-                    counter = 0
-
-                    print('\nExpression Error!')
-
-                    break
-
+        calculation(raw_expression)
+        
     else:
 
         print('\nExpression Error!')
 
         continue
-
-
