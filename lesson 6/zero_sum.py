fbc148323f3680910d8744c068add3e8dd800653
Oleg Kit <oleg2448@gmail.com>
2018-06-11 17:06:56 +0300

    Rewrite with itertools.combinations

--- lesson 6/zero_sum.py
+++ lesson 6/zero_sum.py
@@ -1,15 +1,11 @@
 class zero_sum:
 
     def __init__(self, input_list):
-        a = input_list
+        seq = input_list
         result = []
-        for i in range(len(a)):
-            for j in range(len(a)):
-                for k in range(len(a)):
-                    if j > i:
-                        if k > j:
-                            if a[i] + a[j] + a[k] == 0:
-                                result.append([a[i], a[j], a[k]])
+        for combination in itertools.combinations(seq, 3):
+            if sum(combination) == 0 and list(combination) not in result:
+                result.append(list(combination))
         print(result)
 
 a = zero_sum([-25, -9, -7, -3, 2, 1, 8, 10])
