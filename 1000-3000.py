4008a36b66ae8040895bdb343a6a87e042121819
IvkoVitalik <ivkovitalik@gmail.com>
2018-06-19 23:19:00 +0300

    1000-3000 dz

---
+++ 1000-3000.py
@@ -0,0 +1,11 @@
+for i in range(1000,3001):
+    i = str(i)
+    if int(i[0])%2==0:
+        if int(i[1])%2==0:
+            if int(i[2])%2==0:
+                if int(i[3])%2==0:
+                    print(i)
+                
+    
+        
+        
